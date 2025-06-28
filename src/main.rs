d48345768d0afc3eb1d35f4d8e7a01f54a173ce3
tunamaguro <contact@tunamaguro.dev>
2025-06-29 00:32:18 +0900

    feat: gpio toggle

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,17 @@
 use bsp::entry;
 use defmt::*;
 use defmt_rtt as _;
-use embedded_hal::digital::OutputPin;
+use embedded_hal::{digital::OutputPin, pwm::SetDutyCycle};
 use panic_probe as _;
 
 // Provide an alias for our BSP so we can switch targets quickly.
 // Uncomment the BSP you included in Cargo.toml, the rest of the code does not need to change.
-use rp_pico as bsp;
+use rp_pico::{self as bsp, pac::PWM};
 // use sparkfun_pro_micro_rp2040 as bsp;
 
 use bsp::hal::{
     clocks::{init_clocks_and_plls, Clock},
-    pac,
+    pac, pwm,
     sio::Sio,
     watchdog::Watchdog,
 };
@@ -53,24 +53,24 @@ fn main() -> ! {
         &mut pac.RESETS,
     );
 
-    // This is the correct pin on the Raspberry Pico board. On other boards, even if they have an
-    // on-board LED, it might need to be changed.
-    //
-    // Notably, on the Pico W, the LED is not connected to any of the RP2040 GPIOs but to the cyw43 module instead.
-    // One way to do that is by using [embassy](https://github.com/embassy-rs/embassy/blob/main/examples/rp/src/bin/wifi_blinky.rs)
-    //
-    // If you have a Pico W and want to toggle a LED with a simple GPIO output pin, you can connect an external
-    // LED to one of the GPIO pins, and reference that pin here. Don't forget adding an appropriate resistor
-    // in series with the LED.
-    let mut led_pin = pins.led.into_push_pull_output();
+    let mut stepper_dir = pins.gpio15.into_push_pull_output();
+    stepper_dir.set_high().unwrap();
+
+    // let pwm_slices = pwm::Slices::new(pac.PWM, &mut pac.RESETS);
+    // let mut pwm7 = pwm_slices.pwm7;
+    // pwm7.set_ph_correct();
+    // pwm7.enable();
+
+    // let mut stepper_pwm = pwm7.channel_a;
+    // stepper_pwm.output_to(pins.gpio14);
+
+    let mut stepper_pin = pins.gpio14.into_push_pull_output();
 
     loop {
-        info!("on!");
-        led_pin.set_high().unwrap();
-        delay.delay_ms(500);
-        info!("off!");
-        led_pin.set_low().unwrap();
-        delay.delay_ms(500);
+        stepper_pin.set_high().unwrap();
+        delay.delay_ms(100);
+        stepper_pin.set_low().unwrap();
+        delay.delay_ms(100);
     }
 }
 
